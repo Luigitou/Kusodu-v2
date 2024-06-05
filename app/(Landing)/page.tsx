e6b5dc512d0f250f1e97554e9063544c2465d4d1
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-06-06 00:48:23 +0200

    folder structure done

---
+++ app/(Landing)/page.tsx
@@ -0,0 +1,12 @@
+export default function Landing({}) {
+    return (
+        <div
+            className={
+                'flex h-full w-full flex-col items-center justify-center'
+            }
+        >
+            <h1 className={'text-3xl font-bold text-primary'}>Kusodu</h1>
+            <p className={'text-lg text-primary'}>The modern way to sudoku !</p>
+        </div>
+    );
+}
