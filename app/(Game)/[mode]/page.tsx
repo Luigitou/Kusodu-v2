8edc0447c62498fd637c63a720447cbf365b4c66
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-07-16 20:54:52 +0200

    game menu basic

--- app/(Game)/[mode]/page.tsx
+++ app/(Game)/[mode]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useParams } from 'next/navigation';
+import { Difficulty } from '@/components';
 
 export default function Page() {
     const params = useParams();
@@ -10,15 +11,44 @@ export default function Page() {
     return (
         <div className={'flex h-full w-full items-center justify-center'}>
             <div
-                className={' rounded-lg bg-brighter p-16 text-center shadow-lg'}
+                className={
+                    'flex w-3/5 flex-col items-center gap-16 rounded-lg bg-brighter p-16 text-center shadow-lg'
+                }
             >
                 <div
                     className={
                         'flex flex-col items-center justify-center gap-3 text-white'
                     }
                 >
-                    <h2 className={'text-2xl'}>Difficulty selector</h2>
+                    <h2 className={'text-2xl'}>Game settings</h2>
                     <h3 className={'text-xl'}>{mode}</h3>
+                </div>
+                <div className={'flex w-full gap-4 text-lg'}>
+                    {availableDifficulty.map((difficulty) => (
+                        <Difficulty
+                            key={difficulty.id}
+                            difficulty={difficulty}
+                        />
+                    ))}
+                </div>
+                <div
+                    className={
+                        'group relative w-fit cursor-pointer overflow-hidden'
+                    }
+                >
+                    <div
+                        className={
+                            'absolute right-full top-0 h-full w-full bg-action transition-transform ' +
+                            'rounded-lg duration-300 ease-in-out group-hover:translate-x-full'
+                        }
+                    ></div>
+                    <button
+                        className={
+                            'rounded-lg border-2 border-action px-6 py-2 text-xl text-white group-hover:text-dark'
+                        }
+                    >
+                        Start game !
+                    </button>
                 </div>
             </div>
         </div>
--- components/GameParameters/Difficulty.tsx
+++ components/GameParameters/Difficulty.tsx
@@ -1,3 +1,21 @@
-export function Difficulty({}) {
-    return <div>Difficulty selector</div>;
+type DifficultyProps = {
+    difficulty: {
+        id: number;
+        label: string;
+    };
+};
+
+export function Difficulty({ difficulty }: DifficultyProps) {
+    return (
+        <div className={'group h-20 w-1/5'}>
+            <button
+                className={
+                    'box-border flex h-full w-full  cursor-pointer items-center justify-center rounded-lg ' +
+                    'border-action bg-dark text-white transition-transform duration-200 ease-in-out group-hover:-translate-y-2 group-hover:border-2'
+                }
+            >
+                {difficulty.label}
+            </button>
+        </div>
+    );
 }
